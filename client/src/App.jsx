04441cd127de4756109737be020bb23ec54fe4ea
ub39-tech <battumurbaatar39@gmail.com>
2026-01-01 21:41:49 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://impostor-backend-stg4.onrender.com");
 
 export default function App() {
   const [name, setName] = useState("");
